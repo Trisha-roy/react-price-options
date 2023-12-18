ed4aeccbc49badd881c83322fd161ffaf32e6c80
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-12-18 01:11:11 -0400

    Added phones

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import BarChart from './components/BarChart/BarChart'
 import LineChart from './components/LineChart/LineChart'
 // import DaisyNav from './components/DaisyNav/DaisyNav'
 import NavBar from './components/NavBar/NavBar'
+import Phones from './components/Phones/Phones'
 import PriceOptions from './components/PriceOptions/PriceOptions'
 
 
@@ -17,6 +18,7 @@ function App() {
       <PriceOptions></PriceOptions>
       <LineChart></LineChart>
       <BarChart></BarChart>
+      <Phones></Phones>
      
     </>
   )
