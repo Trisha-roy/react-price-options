3c33b51ebbcb4a7ebb7ecf78c57f8b466f987130
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-12-18 01:47:05 -0400

    Added loader

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar,  XAxis, YAxis } from 'recharts';
+
+import { Audio } from  'react-loader-spinner';
 // import axios from "axios";
 
 
 const Phones = () => {
     const[phones,setPhones]=useState([])
+    const [loading,setLoading]=useState(true)
 
     useEffect(()=>{
         fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
@@ -20,12 +23,25 @@ const Phones = () => {
             })
             console.log(phonesWithFakeData)
             setPhones(phonesWithFakeData)
+            setLoading(false)
         })
         // axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
         // .then(data=>console.log(data.data.data))
     },[])
     return (
         <div>
+
+{ loading &&
+    <Audio
+    height = "80"
+    width = "80"
+    radius = "9"
+    color = 'green'
+    ariaLabel = 'three-dots-loading'     
+    wrapperStyle
+    wrapperClass
+  />
+}
             <h2 className='text-5xl'>Phones: {phones.length}</h2>
             <BarChart width={1200} height={400} data={phones}>
           <Bar dataKey="price" fill="#8884d8" />
